e7b010fa9bdab5671a7280df9637ecc66b6d650b
Elinor <israeli.elinor@gmail.com>
2024-10-09 14:10:27 +0300

    Initial commit

---
+++ cmps/UserPreview.jsx
@@ -0,0 +1,10 @@
+
+export function UserPreview() {
+    const user = { fullname: 'Puki Reactof', score: 87 }
+    
+    return (
+        <section className="user-preview">
+            <h3>BOOKS</h3>
+        </section>
+    )
+}
